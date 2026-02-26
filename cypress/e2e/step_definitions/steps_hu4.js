7a47e186d8a7bb933a2513663941d2b805351d4f
Ivan Sanchez <isanchfo@emeal.nttdata.com>
2026-02-25 22:52:57 -0500

    test(e2e): remove duplicate 'ver la sección' step to fix cypress multiple matching steps error

--- cypress/e2e/step_definitions/steps_hu4.js
+++ cypress/e2e/step_definitions/steps_hu4.js
@@ -4,9 +4,7 @@ Given("que el usuario entra a la barbería online", () => {
     cy.visit("/");
 });
 
-Then("debería ver la sección {string}", (section) => {
-    cy.contains('h3', section).should("be.visible");
-});
+
 
 Then("el barbero {string} debería estar listado", (barberName) => {
     cy.contains('h4', barberName).should("be.visible");
