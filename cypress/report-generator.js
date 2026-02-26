a589c51cdc1bd8cbe5a9454d5ffb75cd8169cd02
Ivan Sanchez <isanchfo@emeal.nttdata.com>
2026-02-25 20:21:55 -0500

    chore: prepare environment for BDD and Codespaces

---
+++ cypress/report-generator.js
@@ -0,0 +1,25 @@
+const report = require("multiple-cucumber-html-reporter");
+
+report.generate({
+    jsonDir: "cypress/reports",
+    reportPath: "cypress/reports",
+    metadata: {
+        browser: {
+            name: "chrome",
+            version: "100",
+        },
+        device: "Local test machine",
+        platform: {
+            name: "ubuntu",
+            version: "20.04",
+        },
+    },
+    customData: {
+        title: "Run info",
+        data: [
+            { label: "Project", value: "Vibrans Barbershop" },
+            { label: "Release", value: "0.1.0" },
+            { label: "Execution Start Time", value: new Date().toISOString() },
+        ],
+    },
+});
