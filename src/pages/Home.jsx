3f263216f3b3082213e6ded6dc5a9ddebf4d2ab8
Ivan Sanchez <isanchfo@emeal.nttdata.com>
2026-02-25 22:39:30 -0500

    fix(home): add Waitlist components to fix HU1 e2e tests

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Hero } from '../components/home/Hero';
 import { Services } from '../components/home/Services';
 import { Barbers } from '../components/home/Barbers';
 import { Memberships } from '../components/home/Memberships';
+import { WaitlistCounter, OnlineCheckIn } from '../components/home/Waitlist';
 import { Footer } from '../components/layout/Footer';
 
 const Home = () => {
@@ -12,6 +13,12 @@ const Home = () => {
             <Header />
             <main>
                 <Hero />
+                <section className="container mx-auto px-6 py-24 border-t border-white/5">
+                    <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 max-w-7xl mx-auto">
+                        <WaitlistCounter />
+                        <OnlineCheckIn />
+                    </div>
+                </section>
                 <Services />
                 <Barbers />
                 <Memberships />
